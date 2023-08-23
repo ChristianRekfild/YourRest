55ea1a51c0300fac536a2f55dbced82dfa590336
M K <kuzz.ma@yandex.ru>
2023-08-23 11:02:31 +0300

    Добавил City

---
+++ YourRest.DAL.Postgre/Entities/City.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourRest.DAL.Postgre.Entities
+{
+    internal class City
+    {
+    }
+}
