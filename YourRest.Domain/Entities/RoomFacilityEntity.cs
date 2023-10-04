38f5d08264ee91f9aba843b5057bcf7b9e5d1d3e
Yaroslav Gritsishin <grits.developer@gmail.com>
2023-10-04 19:42:25 +0300

    Добавил сущьность RoomFacility

---
+++ YourRest.Domain/Entities/RoomFacilityEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace YourRest.Domain.Entities
+{
+    public class RoomFacilityEntity: IntBaseEntity
+    {
+        public string FacilityName { get; set; }
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+    }
+}
