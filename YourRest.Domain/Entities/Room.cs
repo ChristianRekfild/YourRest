e066c1d52c9553816ecc960d709bbce7c6990404
Yaroslav Gritsishin <grits.developer@gmail.com>
2023-10-04 19:46:03 +0300

    Добавил связь один ко многим (одна комната(room) много удобств(facilities)

--- YourRest.Domain/Entities/Room.cs
+++ YourRest.Domain/Entities/Room.cs
@@ -2,6 +2,8 @@
 {
     public class Room : IntBaseEntity
     {
+        public Room() => RoomFacilities = new List<RoomFacilityEntity>();
         public string Name { get; set; }
+        public ICollection<RoomFacilityEntity> RoomFacilities { get; set; }
     }
 }
