5dbf57d29a468ddaea2a7ed08b02f2fd7cb57e88
lilia <lilia_retsya@rightperception.expert>
2023-10-15 23:25:19 +0300

    YouRest-63 Returned warning disable comment

--- YourRest.WebApi/Program.cs
+++ YourRest.WebApi/Program.cs
@@ -47,12 +47,13 @@ public class Program
 
     public static void Configure(IApplicationBuilder app)
     {
+#pragma warning disable CS8604// Code that generates warning CS8604 is written here and will be ignored by the compiler.
         if (app.ApplicationServices.GetService<IWebHostEnvironment>().IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
+#pragma warning disable CS8604// Code that generates warning CS8604 is written here and will be ignored by the compiler.
         app.UseHttpsRedirection();
         app.UseRouting(); // This is necessary for the endpoints to work.
         app.UseAuthorization();
