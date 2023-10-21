e8664a549a45c9bcb5a8e8aa8c0967d27e836e96
Андрей <ansviridov@yandex.ru>
2023-10-21 03:06:16 +0300

    Added jwt token

--- YourRest.ClientWebApp/Program.cs
+++ YourRest.ClientWebApp/Program.cs
@@ -1,7 +1,44 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+// условная бд с пользователями
+var people = new List<Person>
+ {
+    new Person("tom@gmail.com", "12345"),
+    new Person("bob@gmail.com", "55555")
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// добавление сервисов аутентификации
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)  // схема аутентификации - с помощью jwt-токенов
+    .AddJwtBearer(options =>      // подключение аутентификации с помощью jwt-токенов
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // указывает, будет ли валидироваться издатель при валидации токена
+            ValidateIssuer = true,
+            // строка, представляющая издателя
+            ValidIssuer = AuthOptions.ISSUER,
+            // будет ли валидироваться потребитель токена
+            ValidateAudience = true,
+            // установка потребителя токена
+            ValidAudience = AuthOptions.AUDIENCE,
+            // будет ли валидироваться время существования
+            ValidateLifetime = true,
+            // установка ключа безопасности
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            // валидация ключа безопасности
+            ValidateIssuerSigningKey = true,
+        };
+    });
+builder.Services.AddAuthorization();            // добавление сервисов авторизации
 
 var app = builder.Build();
 
@@ -16,12 +53,56 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseAuthentication();   // добавление middleware аутентификации 
+app.UseAuthorization();   // добавление middleware авторизации 
+
 app.UseRouting();
 
-app.UseAuthorization();
+app.Map("/login/{username}", (string username) =>
+//app.Map("/login", (Person loginData) =>
+{
+    //// находим пользователя 
+    //Person? person = people.FirstOrDefault(p => p.Email == loginData.Email && p.Password == loginData.Password);
+    //// если пользователь не найден, отправляем статусный код 401
+    //if (person is null) return Results.Unauthorized();
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+    // создаем JWT-токен
+    var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+    //"Authorization": "Bearer " + token  // token - полученный ранее jwt-токен
+    return new JwtSecurityTokenHandler().WriteToken(jwt);
+    //var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    //// формируем ответ
+    //var response = new
+    //{
+    //    access_token = encodedJwt,
+    //    username = person.Email
+    //};
+
+    //return Results.Json(response);
+});
+
+app.Map("/data", [Authorize] (HttpContext context) => $"Hello World!");
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+public class AuthOptions
+{
+    public const string ISSUER = "MyAuthServer"; // издатель токена
+    public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+    const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
+record class Person(string Email, string Password);
