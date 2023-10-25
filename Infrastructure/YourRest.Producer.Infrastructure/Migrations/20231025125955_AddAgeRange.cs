af48ff39c6f7210e278a5daa915040324d1204cf
M K <kuzz.ma@yandex.ru>
2023-10-25 16:02:00 +0300

    Add AgeRange Migration

---
+++ Infrastructure/YourRest.Producer.Infrastructure/Migrations/20231025125955_AddAgeRange.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace YourRest.Producer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAgeRange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AgeRanges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AgeFrom = table.Column<int>(type: "integer", nullable: false),
+                    AgeTo = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AgeRanges", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AgeRanges");
+        }
+    }
+}
