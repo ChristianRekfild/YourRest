c06336f6f0cc1277725bf26ba8e98a144a45fd71
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2023-11-12 21:06:10 +0300

    Добавил карточки отелей

---
+++ YourRest.ClientWebApp/Views/Components/Card.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace YourRest.ClientWebApp.Views.Components
+{
+    [ViewComponent]
+    public class Card
+    {
+        public IViewComponentResult Invoke()
+        {
+            return new HtmlContentViewComponentResult(
+                new HtmlString(""));
+        }
+    }
+}
