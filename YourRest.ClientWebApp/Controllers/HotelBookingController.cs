a5d1cc11baa0b1553bd4585e787f8ef27153d534
M K <kuzz.ma@yandex.ru>
2023-11-12 21:00:33 +0300

    test

---
+++ YourRest.ClientWebApp/Controllers/HotelBookingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourRest.ClientWebApp.Models;
+using YourRest.Domain.Entities;
+
+namespace YourRest.ClientWebApp.Controllers
+{
+    public class HotelBookingController : Controller
+    {
+
+        public IActionResult index(HotelBookingClientModel booking)
+        {
+
+            return View();
+        }
+        public IActionResult Booking()
+        {
+            HotelBookingClientModel newBooking = new HotelBookingClientModel();
+
+            Console.WriteLine($"Пришла бронь для отеля {newBooking.AccommodationId} комната {newBooking.RoomId}" +
+            $"забронирована с {newBooking.DateFrom} по {newBooking.DateFrom}");
+
+            return View();
+        }
+    }
+}
---
+++ YourRest.ClientWebApp/Models/HotelBookingClientModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace YourRest.ClientWebApp.Models
+{
+    public class HotelBookingClientModel
+    {
+        
+        public int AccommodationId { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public int RoomId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int AdultNr { get; set; }
+        public int ChildrenNr { get; set; }
+    }
+}
