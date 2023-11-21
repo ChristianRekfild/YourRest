7028695615e35c194a01bde42f55f508598eb994
YaroslavGritsishin <59379844+YaroslavGritsishin@users.noreply.github.com>
2023-11-21 21:40:52 +0300

    You rest no 115 add user interface of client web app (#121)
    * Добавил карточки отелей
    
    * Добавил формы для входа, регистрации и поиска
    
    * Создал numeric контрол
    
    * Добавил компоненты Blazor
    
    * Добавил компонент с рекомедациями
    
    * Добавил контента на главную страничку
    
    * You Rest Client MVP v.0.1

---
+++ YourRest.ClientWebApp/Views/Shared/Components/SearchComponent.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using NuGet.Packaging.Licenses;
+using YourRest.ClientWebApp.Models;
+
+namespace YourRest.ClientWebApp.Views.Shared.Components
+{
+    public partial class SearchComponent
+    {
+        private IJSObjectReference module;
+        [Inject] IJSRuntime JSRuntime { get; set; }
+        [Parameter]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        [Parameter]
+        public DateTime EndDate { get; set; } = DateTime.Now.AddDays(3);
+        [Parameter]
+        public int AdultNumber { get; set; } = 2;
+        [Parameter]
+        public int ChildrenNumber { get; set; } = 2;
+
+        [Parameter]
+        public int RoomCount { get; set; } = 1;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+           module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Views/Shared/Components/SearchComponent.razor.js");
+           await module.InvokeAsync<SelectedDateViewModel>("ShowDataPicker", StartDate.ToString("dd.MM.yyyyy"), EndDate.ToString("dd.MM.yyyyy"));
+        }
+        private async Task SetData()
+        {
+            var selectedDate = await module.InvokeAsync<SelectedDateViewModel>("GetData");
+            StartDate = selectedDate.StartDate; 
+            EndDate = selectedDate.EndDate;
+        }
+        private async Task GetData()
+        {
+            var selectedDate = await module.InvokeAsync<SelectedDateViewModel>("GetData");
+            StartDate = selectedDate.StartDate;
+            EndDate = selectedDate.EndDate;
+        }
+    }
+}
