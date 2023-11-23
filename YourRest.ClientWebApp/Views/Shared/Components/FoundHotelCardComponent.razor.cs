4dea010530a1c6c423c12743e673b13a7c75de2a
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2023-11-24 02:48:22 +0300

    Закончил верстку карточки рузультата поиска

--- YourRest.ClientWebApp/Views/Shared/Components/FoundHotelCardComponent.razor.cs
+++ YourRest.ClientWebApp/Views/Shared/Components/FoundHotelCardComponent.razor.cs
@@ -4,9 +4,11 @@ namespace YourRest.ClientWebApp.Views.Shared.Components
 {
     public partial class FoundHotelCardComponent
     {
-        [Parameter] public int StarCount { get; set; } = 5;
-        [Parameter] public double Rating { get; set; } = 9.2;
-        [Parameter] public string Title { get; set; } = "Русотель";
-        [Parameter] public string Description { get; set; } = "Этот уютный отель расположен в 10 минутах ходьбы от железнодорожной станции Битца, в 12 км от парка \"Битцевский лес\" и в 14 км от музея-заповедника \"Царицыно\" с дворцом XVIII века.";
+        [Parameter] public int StarCount { get; set; } 
+        [Parameter] public double Rating { get; set; }
+        [Parameter] public string Title { get; set; } 
+        [Parameter] public string Description { get; set; } 
+        [Parameter] public double Price { get; set; }
+        [Parameter] public string RoomUrl { get; set; }
     }
 }
