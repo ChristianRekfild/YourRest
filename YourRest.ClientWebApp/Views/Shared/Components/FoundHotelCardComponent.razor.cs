336e192b58a84707ef2d1d8ce34848c3c225a7e3
YaroslavGritsishin <59379844+YaroslavGritsishin@users.noreply.github.com>
2023-11-28 00:51:30 +0300

    YouRest No 128 Add found hotel card (#130)
    * Добавил страницу с результатами поиска
    
    * Декомпозировал файл со стилями
    
    * Добавил карточку поиска отеля
    
    * Закончил верстку карточки рузультата поиска
    
    * Добавил фильтер компонент

---
+++ YourRest.ClientWebApp/Views/Shared/Components/FoundHotelCardComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace YourRest.ClientWebApp.Views.Shared.Components
+{
+    public partial class FoundHotelCardComponent
+    {
+        [Parameter] public int StarCount { get; set; } 
+        [Parameter] public double Rating { get; set; }
+        [Parameter] public string Title { get; set; } 
+        [Parameter] public string Description { get; set; } 
+        [Parameter] public double Price { get; set; }
+        [Parameter] public string RoomUrl { get; set; }
+    }
+}
