468b9d6e925f869815d8721519ceed41674eccc6
YaroslavGritsishin <59379844+YaroslavGritsishin@users.noreply.github.com>
2023-11-28 00:40:38 +0300

    YouRest No 126 Add search result page (#129)
    * Добавил страницу с результатами поиска
    
    * Декомпозировал файл со стилями
    
    * add refference
    
    * Fix bug

--- YourRest.ClientWebApp/Views/Shared/Components/SearchComponent.razor.cs
+++ YourRest.ClientWebApp/Views/Shared/Components/SearchComponent.razor.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using NuGet.Packaging.Licenses;
 using YourRest.ClientWebApp.Models;
 
 namespace YourRest.ClientWebApp.Views.Shared.Components
@@ -9,6 +8,7 @@ namespace YourRest.ClientWebApp.Views.Shared.Components
     {
         private IJSObjectReference module;
         [Inject] IJSRuntime JSRuntime { get; set; }
+        [Inject] NavigationManager Navigation { get; set; }
         [Parameter]
         public DateTime StartDate { get; set; } = DateTime.Now;
         [Parameter]
@@ -23,13 +23,28 @@ namespace YourRest.ClientWebApp.Views.Shared.Components
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-           module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Views/Shared/Components/SearchComponent.razor.js");
-           await module.InvokeAsync<SelectedDateViewModel>("ShowDataPicker", StartDate.ToString("dd.MM.yyyy"), EndDate.ToString("dd.MM.yyyy"));
+            module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/components/search_component.js");
+            await module.InvokeAsync<SelectedDateViewModel>("ShowDataPicker", StartDate.ToString("dd.MM.yyyy"), EndDate.ToString("dd.MM.yyyy"));
         }
         private void UpdateSelectedDate(ApplyBtnClickEventArgs e)
         {
             StartDate = e.StartDate;
             EndDate = e.EndDate;
+        }
+        private async Task GetData()
+        {
+            var selectedDate = await module.InvokeAsync<SelectedDateViewModel>("GetData");
+            StartDate = selectedDate.StartDate;
+            EndDate = selectedDate.EndDate;
+            Navigation.NavigateTo("searchresult", true);
+        }
+        private async Task Show()
+        {
+            await module.InvokeVoidAsync("openForm");
+        }
+        private async Task Hide()
+        {
+            await module.InvokeVoidAsync("closeForm");
         }
     }
     public class ApplyBtnClickEventArgs : EventArgs
