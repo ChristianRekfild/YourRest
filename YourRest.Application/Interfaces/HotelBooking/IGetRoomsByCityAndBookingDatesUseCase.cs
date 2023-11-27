950b6fe0d1ae07d3171f52ec9b0fb5c9d33f4791
M K <kuzz.ma@yandex.ru>
2023-11-27 18:37:15 +0300

    t

---
+++ YourRest.Application/Interfaces/HotelBooking/IGetRoomsByCityAndBookingDatesUseCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourRest.Application.Dto;
+using YourRest.Application.Dto.Models.HotelBooking;
+using YourRest.Application.Dto.Models.Room;
+using YourRest.Domain.ValueObjects.Bookings;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace YourRest.Application.Interfaces.HotelBooking
+{
+    public interface IGetRoomsByCityAndBookingDatesUseCase
+    {
+        Task<List<RoomDto>> ExecuteAsync(DateOnly startDay, DateOnly endDay, int cityId , CancellationToken token = default);
+    }
+}
---
+++ YourRest.Application/UseCases/HotelBookingUseCase/GetRoomsByCityAndBookingDatesUseCase.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using YourRest.Application.Dto;
+using YourRest.Application.Dto.Models.HotelBooking;
+using YourRest.Application.Dto.Models.Room;
+using YourRest.Application.Exceptions;
+using YourRest.Application.Interfaces.HotelBooking;
+using YourRest.Domain.Entities;
+using YourRest.Domain.Repositories;
+
+
+namespace YourRest.Application.UseCases.HotelBookingUseCase
+{
+    public class GetRoomsByCityAndBookingDatesUseCase : IGetRoomsByCityAndBookingDatesUseCase
+    {
+        private readonly IBookingRepository bookingRepository;
+        private readonly IRoomRepository roomRepository;
+        private readonly ICustomerRepository customerRepository;
+
+        public GetBookingDatesByRoomIdUseCase(
+            IBookingRepository bookingRepository,
+            IMapper mapper,
+            ICustomerRepository customerRepository,
+            IRoomRepository roomRepository
+            )
+        {
+            this.bookingRepository = bookingRepository;
+            this.roomRepository = roomRepository;
+            this.customerRepository = customerRepository;
+        }
+
+        public async Task<List<RoomOccupiedDateDto>> ExecuteAsync(int RoomId, CancellationToken token = default)
+        {
+            DateOnly dateNow = DateOnly.FromDateTime(DateTime.Today);
+            var roomsExist = await roomRepository.GetAsync(RoomId, token);
+
+            if (roomsExist == null)
+            {
+                throw new InvalidParameterException("Бронируемой комнаты не существует.");
+            }
+ 
+            var bookingList = await bookingRepository.FindAsync(booking => booking.Rooms.Contains(roomsExist) && 
+                booking.EndDate > dateNow, token);
+
+            List<RoomOccupiedDateDto> OccupiedDates = new List<RoomOccupiedDateDto>();
+            foreach ( var booking in bookingList ) 
+            {
+                RoomOccupiedDateDto tempOccupiedDate = new RoomOccupiedDateDto()
+                {
+                    StartDate = booking.StartDate,
+                    EndDate = booking.EndDate
+                };
+                OccupiedDates.Add(tempOccupiedDate);
+            }
+            return OccupiedDates;
+        }
+    }
+}
+
