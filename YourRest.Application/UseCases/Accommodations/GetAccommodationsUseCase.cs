f1814508613c72ec0b3b8769174134e2c86a8ad1
M K <kuzz.ma@yandex.ru>
2024-01-17 21:11:16 +0300

    так же не выдает ту часть данных при запросе

--- YourRest.Application/UseCases/Accommodations/GetAccommodationsUseCase.cs
+++ YourRest.Application/UseCases/Accommodations/GetAccommodationsUseCase.cs
@@ -23,6 +23,51 @@ namespace YourRest.Application.UseCases.Accommodations
             _mapper = mapper;
         }
         public async Task<IEnumerable<AccommodationExtendedDto>> ExecuteAsync(CancellationToken cancellationToken)
-           => _mapper.Map<IEnumerable<AccommodationExtendedDto>>(await _accommodationRepository.GetAllAsync(cancellationToken));      
+        //{
+        //    var accList = await _accommodationRepository.GetAllAsync(cancellationToken);
+        //    List<AccommodationExtendedDto> resultAccExtDtoList = new List<AccommodationExtendedDto>();
+        //    foreach (var acc in accList)
+        //    {
+        //        AccommodationExtendedDto accExtDto = new AccommodationExtendedDto();
+        //        accExtDto.Id = acc.Id;
+        //        accExtDto.Name = acc.Name;
+        //        accExtDto.Address = _mapper.Map<AddressDto>(acc.Address);
+        //        List<RoomWithIdDto> roomsListWithIdDto = new List<RoomWithIdDto>();
+        //        if (accExtDto.Rooms != null)
+        //        {
+        //            foreach (var room in accExtDto.Rooms)
+        //            {
+        //                RoomWithIdDto roomWithIdDto = new RoomWithIdDto();
+        //                roomWithIdDto.Id = room.Id;
+        //                roomWithIdDto.Name = room.Name;
+        //                roomWithIdDto.Capacity = room.Capacity;
+        //                roomWithIdDto.RoomTypeId = room.RoomTypeId;
+        //                roomWithIdDto.SquareInMeter = room.SquareInMeter;
+
+        //                roomsListWithIdDto.Add(roomWithIdDto);
+        //                //roomsListWithIdDto.Add(_mapper.Map<RoomWithIdDto>(room));
+        //            }
+        //        }
+        //        accExtDto.Rooms = roomsListWithIdDto;
+        //        if (accExtDto.Stars != null)
+        //        {
+        //            accExtDto.Stars = acc.StarRating.Stars;
+        //        }
+        //        if (accExtDto.Description != null)
+        //        {
+        //            accExtDto.Description = acc.Description;
+        //        }
+
+        //        if (accExtDto.AccommodationType != null)
+        //        {
+        //            accExtDto.AccommodationType = _mapper.Map<AccommodationTypeDto>(acc.AccommodationType);
+        //        }
+
+        //        resultAccExtDtoList.Add(accExtDto);
+        //    }
+        //    return resultAccExtDtoList;
+        //}
+
+        => _mapper.Map<IEnumerable<AccommodationExtendedDto>>(await _accommodationRepository.GetAllAsync(cancellationToken));
     }
 }
